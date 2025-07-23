9afbb0a618d2cb0d9dfa1b20f3d30fcfa99b6946
KBY\tetep <tetepiti149@gmail.com>
2025-07-23 18:17:14 +0900

    Client에서 전송한, 암호화된 AES Key를 복호화.

--- MiniGameServer/Server/ServerPacketHandler.cpp
+++ MiniGameServer/Server/ServerPacketHandler.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "ServerPacketHandler.h"
 #include "PlayerSession.h"
+#include "ServerGlobal.h"
 
 PacketHandlerFunc GPacketHandler[UINT16_MAX];
 
@@ -13,5 +14,11 @@ bool Handle_INVALID(shared_ptr<PBSession> sessionRef, unsigned char* buffer, int
 bool Handle_C_WELCOME(shared_ptr<PBSession> sessionRef, Protocol::C_Welcome& pkt) {
 	PlayerSession* playerSessionRef = static_cast<PlayerSession*>(sessionRef.get());
 	cout << "C_Welcome 패킷을 받았다." << endl;
-	return false;
+
+	string encryptedStr = pkt.aeskey();
+	vector<unsigned char> encryptedKey(encryptedStr.begin(), encryptedStr.end());
+	vector<unsigned char> AESKey = RSAKeyManager::Decrypt(playerSessionRef->GetRSAKey(), encryptedKey);
+	playerSessionRef->SetAESKey(AESKey);
+
+	return true;
 }
