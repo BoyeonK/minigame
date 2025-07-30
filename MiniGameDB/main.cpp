b75c84550756e99bcb2ca193220b5c43acbc7b94
KBY\tetep <tetepiti149@gmail.com>
2025-07-31 05:32:35 +0900

    코드 인젝션에 대비해서 매개 변수화된 쿼리를 사용하도록 교체중

--- MiniGameDB/main.cpp
+++ MiniGameDB/main.cpp
@@ -6,24 +6,7 @@
 using namespace std;
 
 int main() {
-    wcout.imbue(locale("korean"));
+    
 
-    string a = "alpha";
-    string b = "º£Å¸";
-
-    cout << a << endl;
-    cout << b << endl;
-
-    wstring tableName = L"HandShake";
-    wstring colName = L"value";
-    wstring value = L"10";
-
-    try {
-        wstring q = GDBManager->CreateQuery(tableName, { L"value1", L"value2", L"'10'", L"'20'" });
-        wcout << q << endl;
-    } catch (const runtime_error& e) {
-        cout << e.what() << endl;
-    }
-   
     return 0;
 }
