44013a424025b478c1bceaab8df37dbb52be751c
KBY\tetep <tetepiti149@gmail.com>
2025-08-11 19:55:46 +0900

    바뀐 키보드 입력 방식을 테스트 코드에 적용

--- MiniGameClient/Assets/Scripts/Scenes/LoginScene.cs
+++ MiniGameClient/Assets/Scripts/Scenes/LoginScene.cs
@@ -1,52 +1,64 @@
 using Google.Protobuf.Protocol;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static InputManager;
 
 public class LoginScene : BaseScene {
     protected override void Init() {
         base.Init();
         SceneType = Define.Scene.Login;
         Managers.Resource.Instantiate("UI/Popup/LoginPopup");
+
+        Managers.Input.AddKeyListener(KeyCode.Q, TryConnectToServer, KeyState.Up);
+        Managers.Input.AddKeyListener(KeyCode.W, TryLoginCorrect, KeyState.Up);
+        Managers.Input.AddKeyListener(KeyCode.S, TryLoginWrongPassword, KeyState.Up);
+        Managers.Input.AddKeyListener(KeyCode.X, TryLoginWrongID, KeyState.Up);
+        Managers.Input.AddKeyListener(KeyCode.E, TryCreateAccountCorrect, KeyState.Up);
+        Managers.Input.AddKeyListener(KeyCode.D, TryCreateAccountExisting, KeyState.Up);
     }
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Q))
-            if (!(Managers.Network.IsConnected()))
-                Managers.Network.TryConnectToServer();
-        if (Input.GetKeyDown(KeyCode.W)) {
-            if (Managers.Network.IsConnected() && !(Managers.Network.IsLogined())) {
-                Debug.Log("로그인 시도");
-                C_Encrypted pkt = PacketMaker.MakeCLogin(Managers.Network.GetSession(), "tetepiti149", "qwe123");
-                Managers.Network.Send(pkt);
-            }
+    private void TryConnectToServer() {
+        if (!(Managers.Network.IsConnected()))
+            Managers.Network.TryConnectToServer();
+    }
+
+    private void TryLoginCorrect() {
+        if (Managers.Network.IsConnected() && !(Managers.Network.IsLogined())) {
+            Debug.Log("로그인 시도 (정상)");
+            C_Encrypted pkt = PacketMaker.MakeCLogin(Managers.Network.GetSession(), "tetepiti149", "qwe123");
+            Managers.Network.Send(pkt);
         }
-        if (Input.GetKeyDown(KeyCode.S)) {
-            if (Managers.Network.IsConnected() && !(Managers.Network.IsLogined())) {
-                Debug.Log("로그인 시도");
-                C_Encrypted pkt = PacketMaker.MakeCLogin(Managers.Network.GetSession(), "tetepiti149", "qwe1234");
-                Managers.Network.Send(pkt);
-            }
+    }
+
+    private void TryLoginWrongPassword() {
+        if (Managers.Network.IsConnected() && !(Managers.Network.IsLogined())) {
+            Debug.Log("로그인 시도 (비번틀림)");
+            C_Encrypted pkt = PacketMaker.MakeCLogin(Managers.Network.GetSession(), "tetepiti149", "qwe1234");
+            Managers.Network.Send(pkt);
         }
-        if (Input.GetKeyDown(KeyCode.X)) {
-            if (Managers.Network.IsConnected() && !(Managers.Network.IsLogined())) {
-                Debug.Log("로그인 시도");
-                C_Encrypted pkt = PacketMaker.MakeCLogin(Managers.Network.GetSession(), "tetepiti14", "qwe123");
-                Managers.Network.Send(pkt);
-            }
+    }
+
+    private void TryLoginWrongID() {
+        if (Managers.Network.IsConnected() && !(Managers.Network.IsLogined())) {
+            Debug.Log("로그인 시도 (아이디틀림)");
+            C_Encrypted pkt = PacketMaker.MakeCLogin(Managers.Network.GetSession(), "tetepiti14", "qwe123");
+            Managers.Network.Send(pkt);
         }
-        if (Input.GetKeyDown(KeyCode.E)) {
-            if (Managers.Network.IsConnected() && !(Managers.Network.IsLogined())) {
-                Debug.Log("계정생성 시도 로직이 들어가야 하는 자리");
-                C_Encrypted pkt = PacketMaker.MakeCCreateAccount(Managers.Network.GetSession(), "tetepiti149", "qwe123");
-                Managers.Network.Send(pkt);
-            }
+    }
+
+    private void TryCreateAccountCorrect() {
+        if (Managers.Network.IsConnected() && !(Managers.Network.IsLogined())) {
+            Debug.Log("계정생성 시도 (정상)");
+            C_Encrypted pkt = PacketMaker.MakeCCreateAccount(Managers.Network.GetSession(), "tetepiti149", "qwe123");
+            Managers.Network.Send(pkt);
         }
-        if (Input.GetKeyDown(KeyCode.D)) {
-            if (Managers.Network.IsConnected() && !(Managers.Network.IsLogined())) {
-                Debug.Log("계정생성 시도 로직이 들어가야 하는 자리");
-                C_Encrypted pkt = PacketMaker.MakeCCreateAccount(Managers.Network.GetSession(), "erdfttgg", "qwe123");
-                Managers.Network.Send(pkt);
-            }
+    }
+
+    private void TryCreateAccountExisting() {
+        if (Managers.Network.IsConnected() && !(Managers.Network.IsLogined())) {
+            Debug.Log("계정생성 시도 (이미존재)");
+            C_Encrypted pkt = PacketMaker.MakeCCreateAccount(Managers.Network.GetSession(), "erdfttgg", "qwe123");
+            Managers.Network.Send(pkt);
         }
     }
 
