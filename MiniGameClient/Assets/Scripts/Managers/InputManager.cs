c3040b10b7eb9a63d8d73c8ea88c7c07c5128a47
KBY\tetep <tetepiti149@gmail.com>
2025-08-11 19:27:22 +0900

    클라이언트에서 사용할 InputManager 재설계

--- MiniGameClient/Assets/Scripts/Managers/InputManager.cs
+++ MiniGameClient/Assets/Scripts/Managers/InputManager.cs
@@ -1,22 +1,77 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class InputManager {
-    public Action KeyAction = null;
-    public Action<Define.MouseEvent> MouseAction = null;
+    //키를 누른 시점에 1번, 키를 뗀 시점에 1번, 꾹누르기 가능 여부를 구별하기 위한 enum
+    // 1 = 딸, 2 = 깍, 3 = 꾹
+    public enum KeyState {
+        Down = 0,
+        Up = 1,
+        Press = 2,
+    }
 
+    private class ActionState {
+        public KeyState State;
+        public Action Action;
+    }
+
+    public Action<Define.MouseEvent> MouseAction = null;
+    //특정 Key키(Key가 키임 ㅋㅋㅋㅋㅋ엌ㅋㅋㅋㅋ)에 할당된 여러 델리게이터들을 담을 딕셔너리
+    private Dictionary<KeyCode, List<ActionState>> _keyActions = new Dictionary<KeyCode, List<ActionState>>();
     bool _mousePressed = false;
 
-    public void OnUpdate() {
-        if (Input.anyKey != false && KeyAction != null)
-            KeyAction.Invoke();
+    //구독
+    public void AddKeyListener(KeyCode key, Action action, KeyState state = KeyState.Down) {
+        if (!_keyActions.ContainsKey(key))
+            _keyActions.Add(key, new List<ActionState>());
 
-        //UI를 조작중인 경우, 비활성화.
-        /*
+        _keyActions[key].Add(new ActionState { Action = action, State = state });
+    }
+
+    //구독 취소, 안하면 메모리 누수 생김. object가 파괴되었을 때, 해당 object로 하여금 반드시 실행할 것.
+    public void RemoveKeyListener(KeyCode key, Action action, KeyState state) {
+        if (_keyActions.ContainsKey(key)) {
+            var list = _keyActions[key];
+            // 전달받은 action과 state가 정확히 일치하는 항목을 찾아서 제거
+            var itemToRemove = list.Find(x => x.Action == action && x.State == state);
+            if (itemToRemove != null) {
+                list.Remove(itemToRemove);
+            }
+        }
+    }
+
+    public void OnUpdate() {
+        // 해당 키에 등록된 모든 액션을 순회
+        foreach (var pair in _keyActions) {
+            KeyCode key = pair.Key;
+            var actionList = pair.Value;
+
+            //해당 KeyCode에 등록된 모든 List를 순회, 차례로 딸깍꾹
+            if (Input.GetKeyDown(key)) {
+                foreach (var actionState in actionList) {
+                    if (actionState.State == KeyState.Down)
+                        actionState.Action.Invoke();
+                }
+            }
+            else if (Input.GetKeyUp(key)) {
+                foreach (var actionState in actionList) {
+                    if (actionState.State == KeyState.Up)
+                        actionState.Action.Invoke();
+                }
+            }
+            else if (Input.GetKey(key)) {
+                foreach (var actionState in actionList) {
+                    if (actionState.State == KeyState.Press)
+                        actionState.Action.Invoke();
+                }
+            }
+        }
+
+        //UI에서 자체적으로 마우스 액션을 다룰 예정이기 때문에, 마우스로 UI를 조작중인 경우 마우스 액션 비활성화.
         if (EventSystem.current.IsPointerOverGameObject())
             return;
-        */
 
         if (MouseAction != null) {
             //왼클릭
@@ -35,7 +90,7 @@ public class InputManager {
     }
 
     public void Clear() {
-        KeyAction = null;
+        _keyActions.Clear();
         MouseAction = null;
     }
 }
