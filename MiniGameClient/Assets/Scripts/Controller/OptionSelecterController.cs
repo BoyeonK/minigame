aa37d2d866bde5d3d2359cfe0b5e77dc3890d186
KBY\tetep <tetepiti149@gmail.com>
2025-08-13 18:11:07 +0900

    카메라의 위치, 바라보는 방향 벡터를 조종할 OptionSelecter 컴포넌트 정의

--- MiniGameClient/Assets/Scripts/Controller/OptionSelecterController.cs
+++ MiniGameClient/Assets/Scripts/Controller/OptionSelecterController.cs
@@ -1,82 +1,83 @@
 using UnityEngine;
 
 public class OptionSelecterController : MonoBehaviour {
-    private int _counter = 0;
+    private int _opt = 0;
+    
     private Vector3 _lookDirection;
     private Vector3 _eyePosition;
     private Camera _mainCamera;
     [SerializeField]
     private float _rotationSpeed = 5f;
     [SerializeField]
-    private float _positionSpeed = 5f;
-
-    private void NxtOpt() {
-        _counter = (_counter + 1) % 4;
-        Debug.Log("Next");
-        ChangeLookDir();
-    }
-
-    private void PrOpt() {
-        _counter = (_counter + 3) % 4;
-        Debug.Log("Pre");
-        ChangeLookDir();
-    }
-
+    private float _positionSpeed = 7f;
     void Awake() {
         _mainCamera = Camera.main;
     }
 
+    public void SetOpt(int opt) {
+        _opt = opt;
+        ChangeLookDir();
+    }
+
     private void ChangeLookDir() {
-        switch (_counter) {
+        switch (_opt) {
             case 0:
-                _lookDirection = new Vector3(1.0f, 0.0f, 0f).normalized;
-                _eyePosition = new Vector3(1.0f, 0.0f, 0.0f);
+                _lookDirection = new Vector3(0.0f, 0.0f, 1.0f).normalized;
+                _eyePosition = new Vector3(0.0f, 0.5f, 25.0f);
                 break;
             case 1:
-                _lookDirection = new Vector3(0.0f, 0.0f, 1.0f).normalized;
-                _eyePosition = new Vector3(0.0f, 0.0f, 1.0f);
+                _lookDirection = new Vector3(-0.2f, 0.0f, 1.0f).normalized;
+                _eyePosition = new Vector3(0.0f, 0.5f, 25.0f);
                 break;
             case 2:
-                _lookDirection = new Vector3(-1.0f, 0.0f, 0f).normalized;
-                _eyePosition = new Vector3(-1.0f, 0.0f, 0.0f);
+                _lookDirection = new Vector3(1.0f, 0.0f, 0f).normalized;
+                _eyePosition = new Vector3(0.0f, 0.5f, 25.0f);
                 break;
             case 3:
-                _lookDirection = new Vector3(0.0f, 0.0f, -1.0f).normalized;
-                _eyePosition = new Vector3(0.0f, 0.0f, -1.0f);
+                _lookDirection = new Vector3(-1.0f, 0.0f, -0.2f).normalized;
+                _eyePosition = new Vector3(0.0f, 0.5f, 25.0f);
+                break;
+            case 4:
+                _lookDirection = new Vector3(0.0f, 0.0f, 1.0f).normalized;
+                _eyePosition = new Vector3(0.0f, 0.5f, 1.0f);
+                break;
+            case 5:
+                _lookDirection = new Vector3(0.0f, 0.3f, 1.0f).normalized;
+                _eyePosition = new Vector3(0.0f, 0.5f, 10.0f);
                 break;
         }
     }
 
     //OnEnable과 Start는 비슷하면서도 다르다. 스크립트의 생명주기와 재사용성에 연관이 있다.
     private void OnEnable() {
-        _lookDirection = transform.forward;
-        _eyePosition = new Vector3(0.0f, 0.0f, 0.0f);
-        Managers.Input.AddKeyListener(KeyCode.UpArrow, PrOpt, InputManager.KeyState.Up);
-        Managers.Input.AddKeyListener(KeyCode.DownArrow, NxtOpt, InputManager.KeyState.Up);
+        _opt = 4;
+        ChangeLookDir(); 
     }
 
     //동일하게 OnDisable과 OnDestroy도 그렇다.
     private void OnDisable() {
-        Managers.Input.RemoveKeyListener(KeyCode.UpArrow, PrOpt, InputManager.KeyState.Up);
-        Managers.Input.RemoveKeyListener(KeyCode.DownArrow, NxtOpt, InputManager.KeyState.Up);
+        
     }
 
     void Update() {
         if (_mainCamera != null && _lookDirection != Vector3.zero) {
             Quaternion targetRotation = Quaternion.LookRotation(_lookDirection);
 
-            // 현재 회전에서 목표 회전으로 부드럽게 이동
+            //카메라 위치 이동
+            _mainCamera.transform.position = Vector3.Lerp(
+                _mainCamera.transform.position,
+                _eyePosition,
+                Time.deltaTime * _positionSpeed
+            );
+
+            //카메라가 바라보는 방향 벡터 회전
             _mainCamera.transform.rotation = Quaternion.Slerp(
                 _mainCamera.transform.rotation,
                 targetRotation,
                 Time.deltaTime * _rotationSpeed
             );
 
-            _mainCamera.transform.position = Vector3.Lerp(
-                _mainCamera.transform.position,
-                _eyePosition,
-                Time.deltaTime * _positionSpeed
-            );
+            
         }
     }
 }
