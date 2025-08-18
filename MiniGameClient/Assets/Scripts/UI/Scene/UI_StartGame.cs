57d68d6f8dc3fdb64e65afe1468ab0781f541ad0
Boyeon <tetepiti149@gmail.com>
2025-08-19 07:26:51 +0900

    UI_StartGame 애니메이션 효과 적용

--- MiniGameClient/Assets/Scripts/UI/Scene/UI_StartGame.cs
+++ MiniGameClient/Assets/Scripts/UI/Scene/UI_StartGame.cs
@@ -9,10 +9,25 @@ public class UI_StartGame : UI_Scene {
         Connecting
     }
 
+    enum GameObjects {
+        EnterToStart
+    }
+
     private TextMeshProUGUI _gameName;
     private TextMeshProUGUI _enter;
     private TextMeshProUGUI _toStart;
     private TextMeshProUGUI _connecting;
+    private GameObject _enterToStart;
+
+    private RectTransform _rectGameName;
+    private float _minRectGameNamez = 7.5f;
+    private float _rectSpeed = 2.5f;
+
+    private RectTransform _rectEnterToStart;
+    private float _minEnterPosY = 87.5f;
+    private float _enterPosYSpeed = 2.0f;
+
+    private float _range = 2.5f;
 
     private void OnEnable() {
         Init();
@@ -23,13 +38,27 @@ public class UI_StartGame : UI_Scene {
     }
 
     void Update() {
+        if (_gameName != null && _rectGameName != null) {
+            float pingPongValue = Mathf.PingPong(Time.time * _rectSpeed, _range);
+            float rot = -(_minRectGameNamez + pingPongValue);
+            _rectGameName.localRotation = Quaternion.Euler(0f, 0f, rot);
+        }
 
+        if (_enterToStart != null) {
+            Vector2 pos = _rectEnterToStart.anchoredPosition;
+            float pingPongValue = Mathf.PingPong(Time.time * _enterPosYSpeed, 1.3f * _range);
+            float posY = -(_minEnterPosY + pingPongValue);
+            pos.y = posY;
+            _rectEnterToStart.anchoredPosition = pos;
+        }
     }
 
     public override void Init() {
         base.Init();
         Bind<TextMeshProUGUI>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
         _gameName = Get<TextMeshProUGUI>((int)Texts.GameName);
+        _enterToStart = Get<GameObject>((int)GameObjects.EnterToStart);
         _enter = Get<TextMeshProUGUI>((int)Texts.Enter);
         _toStart = Get<TextMeshProUGUI>((int)Texts.To_Start);
         _connecting = Get<TextMeshProUGUI>((int)Texts.Connecting);
@@ -38,6 +67,13 @@ public class UI_StartGame : UI_Scene {
             cc.a = 0f;
             _connecting.color = cc;
         }
+        if (_gameName != null) { 
+            _rectGameName = _gameName.GetComponent<RectTransform>();
+        }
+        if (_enterToStart != null) {
+            _rectEnterToStart = _enterToStart.GetComponent<RectTransform>();
+        }
+
         Managers.Input.AddKeyListener(KeyCode.Return, TryConnectToServer, InputManager.KeyState.Up);
     }
 
