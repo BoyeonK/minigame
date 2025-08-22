b9307a6a184b67b8140a028232d6e084d17d1a3e
Boyeon <tetepiti149@gmail.com>
2025-08-22 09:10:50 +0900

    UI_EventHandler추가

--- MiniGameClient/Assets/Scripts/UI/UI_EventHandler.cs
+++ MiniGameClient/Assets/Scripts/UI/UI_EventHandler.cs
@@ -2,17 +2,25 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UI_EventHandler : MonoBehaviour, IDragHandler, IPointerClickHandler { 
+public class UI_EventHandler : MonoBehaviour, IDragHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler {
     public Action<PointerEventData> OnDragHandler = null;
     public Action<PointerEventData> OnClickHandler = null;
+    public Action<PointerEventData> OnPointerEnterHandler = null;
+    public Action<PointerEventData> OnPointerExitHandler = null;
 
     public void OnDrag(PointerEventData eventData) {
-        if (OnDragHandler != null)
-            OnDragHandler.Invoke(eventData);
+        OnDragHandler?.Invoke(eventData);
     }
 
     public void OnPointerClick(PointerEventData eventData) {
-        if (OnClickHandler != null)
-            OnClickHandler.Invoke(eventData);
+        OnClickHandler?.Invoke(eventData);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        OnPointerEnterHandler?.Invoke(eventData);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        OnPointerExitHandler?.Invoke(eventData);
     }
 }
