3b7dc396f5aec7a21d77d035fa03f0a6a1571eba
Boyeon <tetepiti149@gmail.com>
2025-09-01 22:18:38 +0900

    서버 : WatingPlayerData의 멤버는 모두 public이므로 변수명 컨벤션 변경

--- MiniGameServer/Server/WatingPlayerData.h
+++ MiniGameServer/Server/WatingPlayerData.h
@@ -2,13 +2,13 @@
 #include "PlayerSession.h"
 
 struct WatingPlayerData {
-	int32_t _elo;
-	weak_ptr<PlayerSession> _playerSessionRef;
+	int32_t elo;
+	weak_ptr<PlayerSession> playerSessionWRef;
 	uint64_t queuedTick = ::GetTickCount64();
 
-	bool IsValidPlayer() const {
-		shared_ptr<PlayerSession> playerSessionRef = _playerSessionRef.lock();
-		if (playerSessionRef == nullptr || playerSessionRef->GetMatchingState() != GameType::None)
+	bool IsValidPlayer(GameType gameType) const {
+		shared_ptr<PlayerSession> playerSessionRef = playerSessionWRef.lock();
+		if (playerSessionRef == nullptr || playerSessionRef->GetMatchingState() != gameType)
 			return false;
 		return true;
 	}
