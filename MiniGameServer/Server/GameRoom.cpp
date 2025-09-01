a946220ccb622191e2fea5b7aa3c48fd18d1b09e
Boyeon <tetepiti149@gmail.com>
2025-09-01 22:28:24 +0900

    서버 : S_KeepAlive패킷을 받은 경우, atomic한 tick갱신

--- MiniGameServer/Server/GameRoom.cpp
+++ MiniGameServer/Server/GameRoom.cpp
@@ -52,8 +52,8 @@ void TestMatchGameRoom::Init2(vector<WatingPlayerData> pdv) {
 
 	if (canStart) {
 		//이제는 정말 게임을 진행할 것임.
-		//지금부터는 해당 플레이어의 게임종료 등의 이유로 세션이 유효하지 않더라도
-		//진행 가능한 방식으로 코드를 작성해야 함.
+		//지금부터는 해당 플레이어의 게임종료 등의 이유로 세션이 유효하지 않더라도 진행 가능한 방식으로 코드를 작성해야 함.
+		//지금부터 연결상태가 좋지 않으면 플레이어 책임으로 간주.
 		_state = GameState::BeforeStart;
 
 		//TODO : S_MatchMakeComplete 패킷을 broadcast해서 Scene변경을 유도 및, 로딩 진행 정도에 따라 C_GameSceneLoadingProgress패킷을 전송받음.
--- MiniGameServer/Server/S2CPacketHandler.cpp
+++ MiniGameServer/Server/S2CPacketHandler.cpp
@@ -214,7 +214,7 @@ bool Handle_C_MatchmakeKeepAlive(shared_ptr<PBSession> sessionRef, S2C_Protocol:
 	shared_ptr<PlayerSession> playerSessionRef = static_pointer_cast<PlayerSession>(sessionRef);
 	if (playerSessionRef == nullptr)
 		return false;
-	/*
+
 	if (playerSessionRef->GetMatchingState() != IntToGameType(pkt.gameid())) {
 		//모든 대기열에서 벗어난 상황임을 통보하고, 대기열 상태를 None으로 바꿈.
 		//클라이언트로 하여금 정상 상태로 돌아갈 수 있도록 노력
@@ -224,7 +224,7 @@ bool Handle_C_MatchmakeKeepAlive(shared_ptr<PBSession> sessionRef, S2C_Protocol:
 		playerSessionRef->SetMatchingState(GameType::None);
 		return false;
 	}
-	*/
+
 	playerSessionRef->SetLastKeepAliveTick(pkt.senttimetick());
 	return true;
 }
