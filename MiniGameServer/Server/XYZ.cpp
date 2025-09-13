427c3f00cd00dfc7e4f629163e24100ab8795d33
Boyeon <tetepiti149@gmail.com>
2025-09-13 13:01:20 +0900

    서버: XYZ를 사용한 벡터 연산 멤버 함수 추가

--- MiniGameServer/Server/XYZ.cpp
+++ MiniGameServer/Server/XYZ.cpp
@@ -6,3 +6,24 @@ void XYZ::SetPosition(float X, float Y, float Z) {
 	y = Y;
 	z = Z;
 }
+
+float XYZ::Magnitude() const {
+	return sqrt(x * x + y * y + z * z);
+}
+
+void XYZ::Normalize() {
+    float mag = Magnitude();
+    if (mag > 0) { // 0으로 나누는 것을 방지
+        x /= mag;
+        y /= mag;
+        z /= mag;
+    }
+}
+
+XYZ XYZ::Normalized() const {
+    float mag = Magnitude();
+    if (mag > 0) 
+        return XYZ(x / mag, y / mag, z / mag);
+
+    return XYZ(0, 0, 0);
+}
--- MiniGameServer/Server/XYZ.h
+++ MiniGameServer/Server/XYZ.h
@@ -2,9 +2,34 @@
 struct XYZ {
 	XYZ(): x(0), y(0), z(0) {};
 	XYZ(float X, float Y, float Z) : x(X), y(Y), z(Z) {};
+
 	void SetPosition(float X, float Y, float Z);
+    float Magnitude() const;
+    void Normalize();
+	XYZ Normalized() const;
+
+    XYZ operator*(float scalar) const {
+        return XYZ(x * scalar, y * scalar, z * scalar);
+    }
+    XYZ operator*(int scalar) const {
+        return XYZ(x * scalar, y * scalar, z * scalar);
+    }
+    XYZ& operator*=(float scalar) {
+        x *= scalar;
+        y *= scalar;
+        z *= scalar;
+        return *this; 
+    }
 
 	float x = 0;
 	float y = 0;
 	float z = 0;
 };
+
+inline XYZ operator*(float scalar, const XYZ& vec) {
+	return vec * scalar;
+}
+
+inline XYZ operator*(int scalar, const XYZ& vec) {
+	return vec * scalar;
+}
