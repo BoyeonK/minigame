3d16bb7d935e6fadebe8a10e1d07287e2d39fa88
Boyeon <tetepiti149@gmail.com>
2025-10-15 18:49:24 +0900

    MPSCQueue구현

--- MiniGameServer/Libraries/MPSCQueue.h
+++ MiniGameServer/Libraries/MPSCQueue.h
@@ -1,7 +1,7 @@
 #pragma once
 
 template<typename T>
-class MPSCQueue {
+class LockQueue {
 public:
 	void Push(T item) {
 		WRITE_RWLOCK;
@@ -33,4 +33,53 @@ public:
 private:
 	USE_RWLOCK;
 	queue<T> _items;
+};
+
+template<typename T>
+class MPSCQueue {
+private:
+    struct Node {
+        Node() = default; 
+        Node(T&& data) : _data(move(data)) { } 
+
+        T _data;
+        std::atomic<Node*> next = nullptr;
+    };
+
+public:
+    MPSCQueue() {
+        Node* dummy = new Node();
+        _head.store(dummy);
+        _tail.store(dummy);
+    }
+
+    ~MPSCQueue() {
+        T out_item;
+        while (TryPop(out_item)) { }
+        delete _head.load();
+    }
+
+    void Push(T item) {
+        Node* newNode = new Node(move(item));
+        Node* prevTail = _tail.exchange(newNode, std::memory_order_acq_rel);
+        prevTail->next.store(newNode, std::memory_order_release);
+    }
+
+    bool TryPop(T& outItem) {
+        Node* head = _head.load(std::memory_order_relaxed);
+        Node* next = head->next.load(std::memory_order_acquire);
+
+        if (next == nullptr) {
+            return false;
+        }
+
+        outItem = move(next->_data);
+        _head.store(next, std::memory_order_release);
+        delete head;
+        return true;
+    }
+
+private:
+    atomic<Node*> _head;
+    atomic<Node*> _tail;
 };
