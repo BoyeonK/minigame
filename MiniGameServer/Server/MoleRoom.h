61d0b0eeb14cd0bbf77bd44de211b8d5342d679c
Boyeon <tetepiti149@gmail.com>
2025-11-24 23:21:35 +0900

    커밋 누락

--- MiniGameServer/Server/MoleRoom.h
+++ MiniGameServer/Server/MoleRoom.h
@@ -34,6 +34,8 @@ public:
 	void CountingPhase();
 	void CalculateGameResult();
 	void UpdateGameResultToDB();
+	void UpdateRecords();
+	void UpdateElos();
 	void EndGame();
 
 	void RenewScoreBoard();
