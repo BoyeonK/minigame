515cd21bc0f3f8572d22eb5c50614981b17762d4
Boyeon <tetepiti149@gmail.com>
2026-01-20 13:33:29 +0900

    두더지잡기 -> 호박쪼개기.

--- MiniGameServer/Server/MoleManager.cpp
+++ MiniGameServer/Server/MoleManager.cpp
@@ -79,6 +79,11 @@ void MoleManager::Update() {
 		return;
 	_lastUpdateRoomTick = now;
 
+	if (now - _lastPendingRoomsVectorAddTick > _pendingRoomsVectorAddTickPeriod) {
+		_lastPendingRoomsVectorAddTick = now;
+		AddRoomsFromPendingVector();
+	}
+
 	{
 		lock_guard<mutex> lock(_roomsLock);
 		for (auto& roomRef : _rooms) {
