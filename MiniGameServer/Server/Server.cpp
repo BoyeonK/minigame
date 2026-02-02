2c075b09cc8b09b57e6792a350c592271721eee5
Boyeon <tetepiti149@gmail.com>
2026-02-02 14:07:24 +0900

    EC2에 서버올리기

--- MiniGameServer/Server/Server.cpp
+++ MiniGameServer/Server/Server.cpp
@@ -6,16 +6,20 @@ int main() {
 	//Game Client와의 프로토콜을 정의한 PacketHandler 초기화.
 	S2CPacketHandler::Init();
 
+	/*
+
 	//DB서버와의 연결 진행
 	DBManager = new DBClientImpl(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
 #ifdef _DEBUG
 	DBManager->HelloAsync();
 #endif
+	*/
 
 	//Client와의 연결을 담당할 서비스 객체 생성 및 Listen시작.
 	GServerService = make_shared<S2CServerServiceImpl>(make_shared<CPCore>(), NetAddress(L"0.0.0.0", 7777), 100);
 	GServerService->StartAccept();
 	
+
 	//Worker Thread 생성
 
 	//Timer Queue를 담당할 worker thread
@@ -26,9 +30,11 @@ int main() {
 		}
 	});
 
+	/*
 	for (auto& gameManager : GGameManagers) {
 		gameManager.second->RenewPublicRecordFromDB();
 	}
+	*/
 
 	//기타 잡무 담당 worker thread.
 	for (int i = 0; i < 5; i++) {
@@ -36,7 +42,7 @@ int main() {
 			while (true) {
 				LEndTickCount = ::GetTickCount64() + 64;
 				ThreadManager::DoGlobalQueueWork();
-				DBManager->AsyncCompleteRpc();
+				//DBManager->AsyncCompleteRpc();
 				GServerService->GetCPCoreRef()->Dispatch(10);
 			}
 		});
@@ -54,6 +60,8 @@ int main() {
 			this_thread::sleep_for(20ms);
 		}
 	});
+
+	cout << "Server is running..." << endl;
 
 	GThreadManager->Join();
 }
