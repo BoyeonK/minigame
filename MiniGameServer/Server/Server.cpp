8ce8d3c635a758c358c9cc32d88539f9b77d9e0f
Boyeon <tetepiti149@gmail.com>
2026-02-05 19:58:39 +0900

    AWS t3.micro기준으로 thread개수 설정

--- MiniGameServer/Server/Server.cpp
+++ MiniGameServer/Server/Server.cpp
@@ -4,9 +4,10 @@
 #include <grpcpp/grpcpp.h>
 
 int main() {
-	//Game Client와의 프로토콜을 정의한 PacketHandler 초기화.
+	//Game Client와의 프로토콜을 정의한 PacketHandler 초기화
 	S2CPacketHandler::Init();
 
+	//DB랑 gRPC통신 SSL인증서 로드
 	string certPath = GEnvManager->GetEnv("CERTNAME");
 	string root_cert = GEnvManager->ReadFile(certPath);
 
@@ -23,19 +24,15 @@ int main() {
 		args
 	);
 
-	//DB서버와의 연결 진행
+	//DB gRPC통신 Channel생성 및 최초 정상 작동 확인하기
 	DBManager = new DBClientImpl(channel);
-
 	DBManager->HelloAsync();
 
 	//Client와의 연결을 담당할 서비스 객체 생성 및 Listen시작.
 	GServerService = make_shared<S2CServerServiceImpl>(make_shared<CPCore>(), NetAddress(L"0.0.0.0", 7777), 100);
 	GServerService->StartAccept();
 	
-
-	//Worker Thread 생성
-
-	//Timer Queue를 담당할 worker thread
+	//Timer Queue만을 담당할 worker thread
 	GThreadManager->Launch([=]() {
 		while (true) {
 			ThreadManager::DoTimerQueueDistribution();
@@ -43,12 +40,14 @@ int main() {
 		}
 	});
 
+	//최고 기록 불러오기 (본디 Redis로 만들어야 했을 부분)
 	for (auto& gameManager : GGameManagers) {
 		gameManager.second->RenewPublicRecordFromDB();
 	}
 
-	//기타 잡무 담당 worker thread.
-	for (int i = 0; i < 5; i++) {
+	//Worker Thread 생성
+	//AWS t3.micro 기준 thread 2개
+	for (int i = 0; i < 2; i++) {
 		GThreadManager->Launch([=]() {
 			while (true) {
 				LEndTickCount = ::GetTickCount64() + 64;
@@ -59,7 +58,7 @@ int main() {
 		});
 	}
 	
-
+	//매치메이킹 및 게임룸 생명주기 관리 담당 Thread
 	GThreadManager->Launch([=]() {
 		while (true) {
 			for (int i = 1; i <= 3; i++) {
